af409c2d0b038f1be160376632e74e0b12c5698a
adrianbbauer <abauer@crsth.com>
2023-11-17 11:34:38 -0500

    feat(styles): updated Header styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,15 +4,15 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header>
-            <Image src="images/logo.png" alt="Annatar Music Logo" />
+        <header className='bg-[#333] text-white p-20 flex justify-between items-center'>
+            <Image src="/images/logo.png" alt="Annatar Music Logo" width={20} height={20} />
             <nav>
-                <ul>
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/about">About</Link></li>
-                    <li><Link href="/services">Services</Link></li>
-                    <li><Link href="/contact">Contact</Link></li>
-                    <li><Link href="/blog">Blog</Link></li>
+                <ul className='list-none flex'>
+                    <li className='mr-20'><Link href="/" className='no-underline'>Home</Link></li>
+                    <li className='mr-20'><Link href="/about" className='no-underline'>About</Link></li>
+                    <li className='mr-20'><Link href="/services" className='no-underline'>Services</Link></li>
+                    <li className='mr-20'><Link href="/contact" className='no-underline'>Contact</Link></li>
+                    <li className='mr-20'><Link href="/blog" className='no-underline'>Blog</Link></li>
                 </ul>
             </nav>
         </header>
