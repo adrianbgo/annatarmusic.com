0632245f781920069fe058ebb78843e84088082e
adrianbgo <adrianbgo@hotmail.com>
2023-11-21 07:54:35 -0500

    feat: added Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-[#333] text-white text-center p-10 fixed w-full bottom-0'>
+        <p>Created by Adrian Bauer</p>
+    </footer>
+  )
+}
+
+export default Footer
